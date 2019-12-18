95643801e8bcd5e6cd15a5c5a9b7b300aff94547
Dmitriy Kanununnikov <dmitriy.kanunnikov@gmail.com>
2019-12-18 19:05:09 +0100

    Add temporary & ugly Api::ApartmentsController with #filter endpoint

---
+++ app/controllers/api/apartments_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  class ApartmentsController < ActionController::API
+    def filter
+      apartments = Apartment.all
+      apartments = apartments.where('LOWER(title) LIKE ?', "%#{apartment_params[:title].downcase}%") if apartment_params[:title].present?
+      apartments = apartments.where('price >= ?', "#{apartment_params[:price_min]}") if apartment_params[:price_min].present?
+      apartments = apartments.where('price <= ?', "#{apartment_params[:price_max]}") if apartment_params[:price_max].present?
+      apartments = apartments.where('sqm >= ?', "#{apartment_params[:sqm_min]}") if apartment_params[:sqm_min].present?
+      apartments = apartments.where('sqm <= ?', "#{apartment_params[:sqm_max]}") if apartment_params[:sqm_max].present?
+      apartments = apartments.where(bedrooms: apartment_params[:bedrooms]) if apartment_params[:bedrooms].present?
+      apartments = apartments.where(bathrooms: apartment_params[:bathrooms]) if apartment_params[:bathrooms].present?
+
+      render json: apartments.sample(9)
+    end
+
+    private
+
+    def apartment_params
+      params.permit(:title, :price_min, :price_max, :sqm_min, :sqm_max, :bedrooms, :bathrooms)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+
+  namespace :api, defaults: { format: 'json' } do
+    get 'filter', to: 'apartments#filter'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
